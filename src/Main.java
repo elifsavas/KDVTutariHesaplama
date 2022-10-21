d52e79342aaed67646fcc2184927d04e89cb354b
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-10-21 23:07:05 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner ;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int fiyat ;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Fiyat Giriniz:");
+        fiyat = input.nextInt();
+
+        double tutar;
+        tutar = fiyat <= 1000 ? (fiyat + (fiyat*0.18)) : (fiyat + (fiyat*0.08)) ;
+        System.out.println("KDV Dahil Fiyat:" + tutar);
+
+        String tutar2 ;
+        tutar2 = fiyat <= 1000 ? "KDV Tutarı : %18" : "KDV Tutarı : %8" ;
+        System.out.println(tutar2);
+
+
     }
 }
